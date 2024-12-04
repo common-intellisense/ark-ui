65bb08229339dc44468f160bdbf78df7cdf8b0be
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-12-04 17:56:36 +0800

    chore: add ark-vue4

--- src/arkVue4/index.ts
+++ src/arkVue4/index.ts
@@ -1,17 +1,16 @@
-import { componentsReducer, propsReducer } from '../../utils'
 import { getComponentsMap, getPropsMap } from './mapping'
 // import directives from '../directives.json'
 
 export function arkVue4() {
-  return propsReducer({
+  return {
     uiName: '@ark-ui/vue4',
     lib: '@ark-ui/vue',
     map: getPropsMap(),
-  })
+  }
 }
 
 export function arkVue4Components() {
-  return componentsReducer({
+  return {
     map: getComponentsMap(),
     isSeperatorByHyphen: false,
     prefix: '',
@@ -19,5 +18,5 @@ export function arkVue4Components() {
     lib: '@ark-ui/vue',
     dynamicLib: '@ark-ui/vue/${name}',
     // directives: directives.arkVue4,
-  })
+  }
 }
